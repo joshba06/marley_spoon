246b0b2fd390024904297265e99c5ce5df83246c
Niklas Bakula <niklasjbakula@gmail.com>
2023-05-28 16:56:16 +0200

    Added basic model tests for recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,6 +5,19 @@ class Recipe < ApplicationRecord
     return "recipe"
   end
 
-  
+  validates :description, presence: true
+  validates :title, presence: true, uniqueness: true
+  validates :chef, presence: true
+  validates :image_url, presence: true
+  validates :contentful_id, presence: true, uniqueness: true
+  validate :tags_validation
+
+  private
+
+  def tags_validation
+    if self.tags != [] && ( self.tags == nil || self.tags.blank? )
+      errors.add(:tags, "cannot be nil or blank")
+    end
+  end
 
 end
